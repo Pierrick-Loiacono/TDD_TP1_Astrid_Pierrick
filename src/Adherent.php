2aa3ee282018b62d3d449e8bd4d9688b95791bc8
Pierrick <pierrick.loiacono@arrobe.fr>
2020-01-29 15:51:35 +0100

    Test 2 : Création de la méthode retournant le nom capitalisé

--- src/Adherent.php
+++ src/Adherent.php
@@ -7,8 +7,16 @@ final class Adherent
     private $prenom;
     private $date_naissance;
 
-    public function __construct()
+
+    public function __construct($nom, $prenom, $date_naissance)
     {
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->date_naissance = $date_naissance;
+    }
+
+    public function getNom(){
+        return strtoupper($this->nom);
     }
 
 
