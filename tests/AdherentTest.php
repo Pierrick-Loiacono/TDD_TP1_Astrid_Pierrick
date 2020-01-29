ab12702602e0354708ee5ff2e974df22c242c526
Pierrick <pierrick.loiacono@arrobe.fr>
2020-01-29 15:50:52 +0100

    Test 2 : Test de la capitalisation du nom

--- tests/AdherentTest.php
+++ tests/AdherentTest.php
@@ -7,10 +7,17 @@ final class AdherentTest extends TestCase
 {
 
     public function testCanBeCreated(){
-        $adherent = new Adherent();
+        $adherent = new Adherent('bossy', 'astrid', '01/01/1999');
         $this->assertInstanceOf(
             Adherent::class,
             $adherent
+        );
+    }
+
+    public function testCreateName(){
+        $adherent = new Adherent('bossy', 'astrid', '01/01/1999');
+        $this->assertEquals('BOSSY',
+            $adherent->getNom()
         );
     }
 
