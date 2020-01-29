eb4f3fa6d2fa6f355ef677aaf46243e5219b1222
Pierrick <pierrick.loiacono@arrobe.fr>
2020-01-29 15:33:40 +0100

    Test 1 : Test de l'instance Adherent

--- tests/AdherentTest.php
+++ tests/AdherentTest.php
@@ -1,11 +1,18 @@
 <?php
+declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 
 final class AdherentTest extends TestCase
 {
 
-
+    public function testCanBeCreated(){
+        $adherent = new Adherent();
+        $this->assertInstanceOf(
+            Adherent::class,
+            $adherent
+        );
+    }
 
 
 }
