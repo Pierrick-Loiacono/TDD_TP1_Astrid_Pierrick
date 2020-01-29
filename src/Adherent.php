42dcaf852efe016397792ec63b734d6edd27b7ab
Pierrick <pierrick.loiacono@arrobe.fr>
2020-01-29 15:35:21 +0100

    Test 1 : Création de la classe Adhérent

---
+++ src/Adherent.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+final class Adherent
+{
+    private $nom;
+    private $prenom;
+    private $date_naissance;
+
+    public function __construct()
+    {
+    }
+
+
+}
